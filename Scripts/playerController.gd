fb6301f8b5ecd7ec109e2cd4dfef19d4447069df
ESPITY <132686177+ESPITY@users.noreply.github.com>
2025-11-10 01:10:18 +0100

    Visibilidad de los propulsores según el movimiento

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -13,6 +13,11 @@ var rotation_direction: float = 0
 var movement_direction: float = 0
 var rotational_velocity:float  = 0
 
+@onready var front_left_propeller = $front_left_propeller
+@onready var front_right_propeller = $front_right_propeller
+@onready var back_left_propeller = $back_left_propeller
+@onready var back_right_propeller = $back_right_propeller
+
 # Rotación y movimiento de la nave con aceleración y fricción
 func movement(delta):
 	# Rotación con deceleración y aceleración
@@ -34,21 +39,69 @@ func movement(delta):
 	else:
 		velocity += transform.x * movement_direction * (vel_acceleration * delta)
 		velocity = velocity.limit_length(max_speed)
+
+# Visibilidad de los propulsores según el movimiento
+func propulsion():
+	if Input.is_action_pressed("up") && Input.is_action_pressed("right"):
+		back_left_propeller.visible = true
+		back_right_propeller.visible = false
+		front_left_propeller.visible = false
+		front_right_propeller.visible = false
 		
+	elif Input.is_action_pressed("up") && Input.is_action_pressed("left"):
+		back_left_propeller.visible = false
+		back_right_propeller.visible = true
+		front_left_propeller.visible = false
+		front_right_propeller.visible = false
+	
+	elif Input.is_action_pressed("down") && Input.is_action_pressed("right"):
+		back_left_propeller.visible = false
+		back_right_propeller.visible = false
+		front_left_propeller.visible = true
+		front_right_propeller.visible = false
+		
+	elif Input.is_action_pressed("down") && Input.is_action_pressed("left"):
+		back_left_propeller.visible = false
+		back_right_propeller.visible = false
+		front_left_propeller.visible = false
+		front_right_propeller.visible = true
+	
+	elif Input.is_action_pressed("up"):
+		back_left_propeller.visible = true
+		back_right_propeller.visible = true
+		front_left_propeller.visible = false
+		front_right_propeller.visible = false
+		
+	elif Input.is_action_pressed("down"):
+		front_left_propeller.visible = true
+		front_right_propeller.visible = true
+		back_left_propeller.visible = false
+		back_right_propeller.visible = false
+		
+	elif Input.is_action_pressed("left"):
+		back_left_propeller.visible = false
+		back_right_propeller.visible = true
+		front_left_propeller.visible = false
+		front_right_propeller.visible = false
+		
+	elif Input.is_action_pressed("right"):
+		back_left_propeller.visible = true
+		back_right_propeller.visible = false
+		front_left_propeller.visible = false
+		front_right_propeller.visible = false
+		
+	else:
+		back_left_propeller.visible = false
+		back_right_propeller.visible = false
+		front_left_propeller.visible = false
+		front_right_propeller.visible = false
+
+	
+# Wrap around de los bordes	
 func teleport():
 	var screen_size = get_viewport_rect().size
 	var sprite_size_x = $Sprite2D.texture.get_width() / 2
 	var sprite_size_y = $Sprite2D.texture.get_height() / 2
-	
-	#if global_position.y < -sprite_size_y:
-		#global_position.y = screen_size.y + sprite_size_y
-	#elif global_position.y > screen_size.y + sprite_size_y:
-		#global_position.y = -sprite_size_y
-	
-	#if global_position.x < -sprite_size_x:
-		#global_position.x = screen_size.x + sprite_size_x
-	#elif global_position.x > screen_size.x + sprite_size_x:
-		#global_position.x = -sprite_size_x
 	
 	position.x = wrapf(position.x, -sprite_size_x, screen_size.x + sprite_size_x)
 	position.y = wrapf(position.y, -sprite_size_y, screen_size.y + sprite_size_y)
@@ -58,3 +111,5 @@ func _physics_process(delta):
 	movement(delta)
 	move_and_slide()
 	teleport()
+	propulsion()
+	print(rotation_direction)
