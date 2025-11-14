7b17e638341d05e3c950b46195f575ad7d5d7dcf
ESPITY <132686177+ESPITY@users.noreply.github.com>
2025-11-14 16:13:57 +0100

    Asteroid spawner

--- Scripts/asteroid.gd
+++ Scripts/asteroid.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+class_name Asteroid extends RigidBody2D
 
 var rng = RandomNumberGenerator.new()
 
@@ -17,7 +17,7 @@ func _ready() -> void:
 	var texture: int
 	match size:
 		asteroid_size.HUGE:
-			speed = randf_range(-50, 50)
+			speed = randf_range(-100, 100)
 			texture = randi_range(1, 4)
 			match texture:
 				1:
@@ -33,7 +33,7 @@ func _ready() -> void:
 					sprite.texture = preload("res://Sprites/Asteroids/Asteroid_Huge-4.png")
 					collision.shape = preload("res://Resources/Asteroid_CS_Huge-4.tres")
 		asteroid_size.BIG:
-			speed = randf_range(-100, 100)
+			speed = randf_range(-150, 150)
 			texture = randi_range(1, 4)
 			match texture:
 				1:
@@ -49,7 +49,7 @@ func _ready() -> void:
 					sprite.texture = preload("res://Sprites/Asteroids/Asteroid_Big-4.png")
 					collision.shape = preload("res://Resources/Asteroid_CS_Big-4.tres")
 		asteroid_size.MEDIUM:
-			speed = randf_range(-150, 150)
+			speed = randf_range(-200, 200)
 			texture = randi_range(1, 2)
 			match texture:
 				1:
@@ -59,7 +59,7 @@ func _ready() -> void:
 					sprite.texture = preload("res://Sprites/Asteroids/Asteroid_Medium-2.png")
 					collision.shape = preload("res://Resources/Asteroid_CS_Medium-2.tres")
 		asteroid_size.SMALL:
-			speed = randf_range(-200, 200)
+			speed = randf_range(-250, 250)
 			texture = randi_range(1, 2)
 			match texture:
 				1:
@@ -69,7 +69,7 @@ func _ready() -> void:
 					sprite.texture = preload("res://Sprites/Asteroids/Asteroid_Small-2.png")
 					collision.shape = preload("res://Resources/Asteroid_CS_Small-2.tres")
 		asteroid_size.TINY:
-			speed = randf_range(-250, 250)
+			speed = randf_range(-300, 300)
 			texture = randi_range(1, 2)
 			match texture:
 				1:
@@ -90,11 +90,43 @@ func _ready() -> void:
 	linear_velocity = Vector2(speed, speed)
 	angular_velocity = randf_range(-1, 1)
 
+func teleport():
+	var screen_size = get_viewport_rect().size
+	var sprite_size = sprite.texture.get_size() / 2
+	
+	global_position.x = wrapf(global_position.x, -sprite_size.x, screen_size.x + sprite_size.x)
+	global_position.y = wrapf(global_position.y, -sprite_size.y, screen_size.y + sprite_size.y)
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	print(linear_velocity)
+	
+func _physics_process(delta):
+	teleport()
 
+func split():
+	# Generamos un número aleatorio de asteroides hijos
+	var num_children = randi_range(2, 4)  # Puede generar entre 2 y 4 asteroides
+
+	# En función del tamaño actual, generamos asteroides más pequeños
+	var new_size: asteroid_size
+	match size:
+		asteroid_size.HUGE:
+			new_size = asteroid_size.BIG
+		asteroid_size.BIG:
+			new_size = asteroid_size.MEDIUM
+		asteroid_size.MEDIUM:
+			new_size = asteroid_size.SMALL
+		asteroid_size.SMALL:
+			new_size = asteroid_size.TINY
+		asteroid_size.TINY:
+			return  # No se puede dividir más
+
+	# Crear los nuevos asteroides
+	#for i in range(num_children):
+		#var new_asteroid = self.new()
+		#new_asteroid.size = new_size
+		#new_asteroid.position = position  # Aparecen en la misma posición del asteroide original
+		#new_asteroid.linear_velocity = rng.randf_range(-100, 100) * Vector2(cos(randf_range(0, 2 * PI)), sin(randf_range(0, 2 * PI)))
+		#get_parent().add_child(new_asteroid)
 
 func explode():
 	queue_free()	
--- Scripts/asteroid_spawner.gd
+++ Scripts/asteroid_spawner.gd
@@ -1,11 +1,29 @@
 extends Node2D
 
+@export var asteroid: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().create_timer(10).timeout
+	spawn_asteroid()
+	await get_tree().create_timer(10).timeout
+	spawn_asteroid()
+	await get_tree().create_timer(10).timeout
+	spawn_asteroid()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func spawn_asteroid():
+	var screen_size = get_viewport_rect().size
+
+	var asteroid_inst = asteroid.instantiate()
+	asteroid_inst.size = Asteroid.asteroid_size.BIG
+	
+	var random_position = Vector2(randf_range(0, screen_size.x), randf_range(0, screen_size.y))
+	
+	asteroid_inst.global_position = random_position
+	self.add_child(asteroid_inst)
